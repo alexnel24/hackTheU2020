80fda578aaa0c4bbff5c68b5d9e0b54fbf1ec846
Alex Nelson <alexander.nelson24@gmail.com>
2020-10-18 16:34:57 -0600

    removing stuff

--- pyCharm/initializeCoach.py
+++
@@ -1,7 +0,0 @@
-from csvDatabaseManager import addToDatabaseCSV
-from galileoRequestFunctions import createCardholder
-
-newId = createCardholder("Quin Snyder", "1980-02-12")
-# newId = 5080
-if newId != None:
-    addToDatabaseCSV(newId, "Quin Snyder", "coach")
